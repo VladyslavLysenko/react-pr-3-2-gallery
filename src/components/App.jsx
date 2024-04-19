414aa33566c80faf87524958b3a0a1ef70bd383f
Nancy311 <105671987+VladyslavLysenko@users.noreply.github.com>
2024-04-19 11:14:43 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
+import { ImageGallery } from "./ImageGallery/ImageGallery";
 import { SearchBar } from "./SearchBar/SearchBar";
 
 export const App = () => {
   return (
     <>
-      <SearchBar/>
+      <SearchBar />
+      <ImageGallery/>
     </>
      
   );
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,8 +1,10 @@
 import css from 'styles.module.css'
-import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
+import { nanoid } from 'nanoid'
+// model.id = nanoid() //=> "V1StGXR8_Z5jdHi6B-myT"
 
 export const ImageGallery = () => {
     return <ul className={css.ImageGallery}>
-        <ImageGalleryItem/>
+        <ImageGalleryItem key={nanoid()}/>
     </ul>;
 };
